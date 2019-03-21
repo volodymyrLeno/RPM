a2795f84b8d7f45b069cd79694f836dacca745ec
volodymyrLeno <31915954+volodymyrLeno@users.noreply.github.com>
2019-03-21 12:40:27 +1100

    Added recording example

--- RPM/src/Main.java
+++ RPM/src/Main.java
@@ -8,11 +8,13 @@ import java.util.List;
 public class Main {
 
     public static void main(String[] args) {
-        String path = new File("src\\logs\\test1.csv").getAbsolutePath();
+        String path = new File("src\\logs\\runningExample.csv").getAbsolutePath();
         List<Event> events = logReader.readCSV(path);
         HashMap<String, List<Event>> cases = segmentator.segment(events);
         System.out.println("\n\n");
         var example = transformationDiscoverer.extractExamples(cases);
+        for(TransformationExample te: example)
+            System.out.println(te);
         transformationDiscoverer.discoverDataTransformations(example);
     }
 }
